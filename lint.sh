fb262a003c100224721009a3185e1d335481a12d
Bowen Tan <bowentan3-c@my.cityu.edu.hk>
2022-09-10 21:47:48 +0800

    Change path

--- lint.sh
+++ lint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cpplint /tmp/lint//tmp/lint/easy/Problem-0001-Two-Sum/answer.cpp
-clang-format --dry-run /tmp/lint/easy/Problem-0001-Two-Sum/answer.cpp
+cpplint easy/Problem-0001-Two-Sum/answer.cpp
+clang-format --dry-run easy/Problem-0001-Two-Sum/answer.cpp
