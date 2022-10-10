8f175abd7c93403d0b331bfe7e5ba4512d3d2017
Bowen Tan <bowentan78@gmail.com>
2022-10-10 13:01:09 +0800

    Bump version: 0.1.1 → 0.1.1

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/bowentan/glob-linters",
-    version="0.1.0",
+    version="version="0.1.1"",
     zip_safe=False,
 )
