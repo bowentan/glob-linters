85c51de9fdbdc2e72c6efc38411623afb4808eaf
Bowen Tan <bowentan78@gmail.com>
2022-10-11 15:39:36 +0800

    chore: change misc
    Signed-off-by: Bowen Tan <bowentan78@gmail.com>

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from setuptools import find_packages, setup
 with open("README.rst", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-with open("CHANGELOG.rst", encoding="utf-8") as history_file:
-    changelog = history_file.read()
+with open("CHANGELOG.rst", encoding="utf-8") as changelog_file:
+    changelog = changelog_file.read()
 
 requirements = [
     "clang-format>=14.0.6",
