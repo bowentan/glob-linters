c49355afe0d95575839dabe823822eef17e170fb
Bowen Tan <bowentan78@gmail.com>
2022-10-11 15:39:42 +0800

    Bump version: 0.1.0-rc.10 → 0.1.0-rc.11
    Signed-off-by: Bowen Tan <bowentan78@gmail.com>

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/bowentan/glob-linters",
-    version="0.1.0-rc.10",
+    version="0.1.0-rc.11",
     zip_safe=False,
 )
