f3d4b4ecb01c861eecf5a433b8da6d1d82a4c0fb
Bowen Tan <bowentan78@gmail.com>
2022-10-13 13:56:19 +0800

    Develop (#54)
    * Bump version: 0.1.0-rc.20 → 0.1.0-rc.21
    
    Signed-off-by: Bowen Tan <bowentan78@gmail.com>
    
    * ci: merge workflows
    
    Signed-off-by: Bowen Tan <bowentan78@gmail.com>
    
    * chore: release 0.1.0-rc.21
    
    Release-As: 0.1.0-rc.21
    Signed-off-by: Bowen Tan <bowentan78@gmail.com>
    
    * ci: revert
    
    Signed-off-by: Bowen Tan <bowentan78@gmail.com>
    
    * Bump version: 0.1.0-rc.21 → 0.1.0-rc.22
    
    Signed-off-by: Bowen Tan <bowentan78@gmail.com>
    
    Signed-off-by: Bowen Tan <bowentan78@gmail.com>

--- setup.py
+++ setup.py
@@ -77,6 +77,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/bowentan/glob-linters",
-    version="0.1.0-rc.21",
+    version="0.1.0-rc.22",
     zip_safe=False,
 )
